6ef5fed584cf5e2e540788453bb736d9932653ae
arthew <tongxunhuan2@sina.com>
2016-06-02 14:53:17 +0800

    修改股指竞猜规则，增加提示

--- eth-wallet/apps/quiza/QuizA.js
+++ eth-wallet/apps/quiza/QuizA.js
@@ -8,6 +8,9 @@ var quizContract;
 var Const_Status = {"0": '竞投中', "1": '已封盘', "2": '已收盘'};
 var registries = {"test": "0x8e65C683Cc8912cAcAE9bc515b38df51d2EEDF26","main": "0x0000000000000000000000000000000000000000"};
 var pendingAmount = [0, 0, 0, 2];
+var targetCode = null;
+var preClosePrice = 0;
+var contractStatus = 2;
 $(function () {
     //Check Login
     if(global.wallet.getPrivateKey() == null) {
@@ -21,23 +24,26 @@ $(function () {
     var zero = web3.toBigNumber(0);
     pendingAmount = [zero, zero, zero, 2];
     //2. 取当前选择的合约缩影
-    contractIndex = window.localStorage.getItem("QuizAContractIndex")
+    contractIndex = window.localStorage.getItem("QuizAContractIndex");
     if (contractIndex == null) {
         contractIndex = 0; //If not set, use default instead
+    }else {
+        contractIndex = parseInt(contractIndex);
     }
     setTimeout(function() {
         $('#coverImg').remove();
+        $('#divTabbar').show();
     },2200);
     registry.contracts(contractIndex, function (e, r) { //Get current quiz contract address
         if (e == null) {
             var address = r[0];
             contract = web3.eth.contract(quizContractABI); //Init ABI
             quizContract = contract.at(address); //Locate quiz contract instance
-            getBaseInfo(function (e, r) { //Fetch base info
+//            getBaseInfo(function (e, r) { //Fetch base info
                 setTimeout(function () {
                     reladInfo();
-                }, 1000);
-                getMyBetInfo();
+                }, 500);
+                //getMyBetInfo();
                 setInterval(reladInfo, 5000);
                 $('a[name=navBetNow]').bind('click', function () {
                     $('#about').hide();
@@ -52,9 +58,22 @@ $(function () {
                             $.alert('系统提示', '<strong>本期竞猜已经结束</strong><p/>请下期再参加<p/>（预计在15分钟内开始）');
                     }
                 })
-            });
+ //           });
         } else {
             $.alert('初始化合约错误', e);
+        }
+    });
+
+    //3. If it's mobile, bind touch move event
+    if(isTouchDevice()) {
+        document.addEventListener('touchstart', touchSatrtFunc, false);
+        document.addEventListener('touchend', touchEndFunc, false);
+    }
+
+    $.get("http://light.hs.net/auth.json", function(r,e) {
+        if(e == null || e=='success') {
+            access_token = r.access_token;
+            setInterval(refreshQuota, 3000);
         }
     });
 });
@@ -96,10 +115,12 @@ function submitBet() {
         if (e == null) {
             $('#betForm').hide();
 
-            if ($('input[name=radioAction]')[0].checked) {
+            if ($('input[name=radioAction]')[0].checked) {//Bet Up
                 pendingAmount[1] = pendingAmount[1].plus(amount);
-            } else {
+                window.localStorage.setItem('tx-betUp-'+ targetCode ,r);
+            } else { //Bet Down
                 pendingAmount[2] = pendingAmount[2].plus(amount);
+                window.localStorage.setItem('tx-betDown-'+ targetCode, r);
             }
             getMyBetInfo();
             setTimeout(function() {$('#loadingToast').hide();},3000);
@@ -123,21 +144,25 @@ function reladInfo() {
 function getBaseInfo(cb) {
     quizContract.baseInfo(function (e1, r1) { //Fetch base info
         if (e1 == null) {
+            targetCode = r1[0];
             $('#targetInfo').text(r1[1] + '(' + r1[0] + ')');
             $('#targetDate').text(r1[2]);
             var price = r1[3].toNumber();
+            preClosePrice = price / 100;
             if (price > 0) {
-                $('#targetPreClose').text(price / 100);
+                $('#targetPreClose').text(preClosePrice);
             } else {
                 $('#targetPreClose').text('-');
             }
             var price = r1[4].toNumber();
             if (price > 0) {
                 $('#targetClose').text(price / 100);
+                $('#lblTargetClose').text('今收盘');
             } else {
-                $('#targetClose').text('-');
+                $('#lblTargetClose').text('最新价');
             }
-            $('#targetStatus').text(Const_Status[r1[5]]);
+            contractStatus = r1[5].toNumber();
+            $('#targetStatus').text(Const_Status[contractStatus]);
 
             pendingAmount[0] = web3.eth.getBalance(global.wallet.getAddressString());
         } else {
@@ -155,7 +180,8 @@ function getSummaryInfo() {
             $('#sumAmountBetUp').text(web3.fromWei(r1[1]).round(2));
             $('#countBetDown').text(r1[2].toNumber());
             $('#sumAmountBetDown').text(web3.fromWei(r1[3]).round(2));
-            $('#targetStatus').text(Const_Status[r1[5]]);
+            contractStatus = r1[5].toNumber();
+            $('#targetStatus').text(Const_Status[contractStatus]);
             if(pendingAmount[3] != r1[5].toNumber()) {
                 setTimeout(function() {
                     getBaseInfo();
@@ -178,14 +204,42 @@ function getSummaryInfo() {
 function getMyBetInfo() {
     quizContract.betRecords(global.wallet.getAddressString(), function (e1, r1) { //Fetch My Bet Record
         if (e1 == null) {
-            $('#myBetUp').text(web3.fromWei(r1[0].plus(pendingAmount[1])).round(2));
-            $('#myBetDown').text(web3.fromWei(r1[1].plus(pendingAmount[2])).round(2));
+            var amount = web3.fromWei(r1[0].plus(pendingAmount[1]));
+            $('#myBetUp').text(amount.round(2));
+
+            var tx = window.localStorage.getItem('tx-betUp-'+ targetCode );
+            if(amount.eq(0)) {
+                window.localStorage.removeItem('tx-betUp-'+ targetCode );
+                tx = null;
+            }
+            if(tx == null) {
+                $('#lblBetUp').html('买涨');
+            }else{
+                $('#lblBetUp').html('<a href="http://testnet.etherscan.io/tx/' + tx +'">买涨</a>' );
+            }
+
+            amount = web3.fromWei(r1[1].plus(pendingAmount[2]));
+            tx = window.localStorage.getItem('tx-betDown-'+ targetCode );
+            if(amount.eq(0)) {
+                window.localStorage.removeItem('tx-betDown-'+ targetCode );
+                tx = null;
+            }
+
+            $('#myBetDown').text(amount.round(2));
+            if(tx == null) {
+                $('#lblBetDown').html('买跌');
+            }else {
+                $('#lblBetDown').html('<a href="http://testnet.etherscan.io/tx/' + tx +'">买跌</a>' );
+            }
+
             $('#myWin').text(web3.fromWei(r1[2]).round(2));
             if (r1[2].gt(0)) {
                 $('#myWin').css('color', 'red');
+                $('#betResult').css('color', 'red');
                 $('#betResult').text('赢');
             } else if (r1[2].lt(0)) {
                 $('#myWin').css('color', 'green');
+                $('#betResult').css('color', 'green');
                 $('#betResult').text('输');
             } else {
                 $('#betResult').text('');
@@ -194,4 +248,91 @@ function getMyBetInfo() {
             $.alert('取合约投注信息错误', e1);
         }
     });
+}
+
+
+//Touch Event
+var startX = 0, startY = 0;
+//touchstart事件
+function touchSatrtFunc(evt) {
+    try
+    {
+        //evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
+        var touch = evt.touches[0]; //获取第一个触点
+        var x = Number(touch.pageX); //页面触点X坐标
+        var y = Number(touch.pageY); //页面触点Y坐标
+        //记录触点初始位置
+        startX = x;
+        startY = y;
+        //console.log('Touch Start: ' + x +', ' + y);
+    }
+    catch (e) {
+        alert('touchSatrtFunc：' + e.message);
+    }
+}
+
+//touchend事件
+function touchEndFunc(evt) {
+    try {
+        //evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
+        var touch = evt.changedTouches[0]; //获取第一个触点
+        var x = Number(touch.pageX); //页面触点X坐标
+        var y = Number(touch.pageY); //页面触点Y坐标
+        //判断滑动方向
+//        console.log('Touch End: ' + x +', ' + y);
+        var xchange = x - startX;
+        if(Math.abs(xchange) > Math.abs(y - startY)) {
+            //Only move dist on x greater than on y
+            if (xchange > 50) {
+                contractIndex = contractIndex -1;
+                if(contractIndex<0) {
+                    contractIndex =registry.size().toNumber() - 1;
+                }
+                window.localStorage.setItem("QuizAContractIndex",contractIndex);
+                evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
+                document.location.reload();
+            }else if(xchange < -50) {
+                contractIndex = contractIndex + 1;
+                if(contractIndex>= registry.size().toNumber()) {
+                    contractIndex = 0;
+                }
+                window.localStorage.setItem("QuizAContractIndex",contractIndex);
+                evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
+                document.location.reload();
+            }
+        }
+
+    }
+    catch (e) {
+        alert('touchEndFunc：' + e.message);
+    }
+}
+
+//判断是否支持触摸事件
+function isTouchDevice() {
+    try {
+        document.createEvent("TouchEvent");
+        return true;
+    }
+    catch (e) {
+        return false;
+    }
+}
+
+function refreshQuota() {
+    if(targetCode !=null && typeof access_token != 'undefined' && contractStatus <2) {
+        $.getJSON('http://open.hscloud.cn/quote/v1/real?en_prod_code=' + targetCode + '&fields=last_px&access_token=' + access_token,function(r,e) {
+            if(e == 'success') {
+                var lastPrice = r.data.snapshot[targetCode][2];
+                if(lastPrice < preClosePrice) {
+                    $('#targetClose').css('color', 'green');
+                    $('#targetClose').text(lastPrice.toFixed(2) + ' ↓');
+                }else {
+                    $('#targetClose').css('color', 'red');
+                $('#targetClose').text(lastPrice.toFixed(2) + ' ↑');
+                }
+
+            }
+        });
+    }
 }
